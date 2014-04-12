a6b3c4dda94d8ca2349d4b258b7fb9466455cbd6
Tomoya Koyanagi <tomk79@gmail.com>
2014-04-13 01:48:46 +0900

    リリース準備。標準出力など親切になった。readme.md も追加。

--- headingImageGenerator.js
+++ headingImageGenerator.js
@@ -1,12 +1,30 @@
+/**
+ * node headingImageGenerator.js
+ * @author Tomoya Koyanagi
+ */
 (function() {
+	console.log('---------');
+	console.log('Starting "headingImageGenerator.js"');
+
+
 	var fs = require('fs');
 	var csv = require('csv');//https://github.com/wdavidw/node-csv
 	var phantom = require('node-phantom');//https://github.com/alexscheelmeyer/node-phantom
 
 	var conf = {
-		// ドキュメントルートのファイルパス
-		documentRoot:'./htdocs/'
+		documentRoot: __dirname+'/htdocs/',// ドキュメントルートのファイルパス
+		pathCsv: __dirname+'/data.csv',// CSVファイルパス
+		pathOutput: __dirname+'/output/',// PNG出力ディレクトリパス
+		port: 80, // nodeサーバーのポート番号
+		unit: 1, // nodeサーバーのポート番号
+		version: '0.0.1-nb' // headingImageGenerator.js のバージョン
 	};
+
+	console.log('version '+conf.version);
+	console.log('script filename = '+__filename);
+	console.log('working dir = '+__dirname);
+	console.log('---------');
+	console.log('');
 
 	var idx = 2;
 	var options = (function(){
@@ -19,10 +37,11 @@
 		return rtn;
 	})();
 
-	if( !options.port ){
-		console.log("[ERROR] No port number given."+"\n");
-		return process.exit();
-	}
+	// オプションをコンフィグに反映
+	if( options.pathCsv )   { conf.pathCsv = options.pathCsv; }
+	if( options.pathOutput ){ conf.pathOutput = options.pathOutput; }
+	if( options.port )      { conf.port = options.port; }
+	if( options.unit )      { conf.unit = options.unit; }
 
 	// --------------------------------------
 	// setup webserver
@@ -89,11 +108,92 @@
 		});
 
 		// 指定ポートでLISTEN状態にする
-		server.listen( options.port );
+		server.listen( conf.port );
 
 	})();
 
-	// process.exit();
+
+
+
+
+
+	var tasks = [];
+	var taskProgress = 0;
+	var done = 0;
+	function loadCSV(){
+		fs.readFile(conf.pathCsv, function(error, csvData){
+			if( error ){
+				console.log('error: '+ conf.pathCsv +' could not open.');
+				process.exit();
+			}
+
+			csv()
+				.from.string(
+					csvData,
+					{}
+				)
+				.to.array(function(data){
+					for( var i in data ){
+						var row = {};
+						var idx = 0;
+						row['label'] = data[i][idx++];
+						row['templateName'] = data[i][idx++]||'index';
+						row['width'] = Number(data[i][idx++]);
+						row['height'] = Number(data[i][idx++]);
+						row['fileName'] = require('crypto').createHash('md5').update(row['label'], 'utf8').digest('hex')+'.png';
+						tasks.push(row);
+					}
+					generateImage();
+				})
+			;
+
+		});
+	}
+
+	function generateImage(){
+		console.log('');
+		console.log('-- processing tasks.');
+
+		while( 1 ){
+			if( tasks.length <= taskProgress ){
+				console.log('All task clear.');
+				console.log('');
+				break;
+			}
+
+			var row = tasks[taskProgress];
+			console.log('    '+row.label);
+
+			webCapFile(
+				row,
+				'http://127.0.0.1:'+conf.port+'/'+row.templateName+'.html?label='+encodeURIComponent( row.label ),
+				conf.pathOutput+'/'+row.fileName,
+				row.width,
+				row.height,
+				row.width,
+				row.height,
+				function(row){
+					console.log('        generating image "' + row.label + '" (as "'+row.fileName+'") done.');
+					done ++;
+					if( done%conf.unit == 0 ){
+						generateImage();
+					}
+					if( tasks.length <= done ){
+						console.log( '' );
+						console.log( '' );
+						console.log( 'Done all.' );
+						console.log( 'exit;'+"\n" );
+						process.exit();
+					}
+				}
+			);
+			taskProgress++;
+			if( taskProgress%conf.unit == 0 ){
+				break;
+			}
+		}
+		return true;
+	}
 
 	/**
 	 * webCapFile
@@ -116,63 +216,14 @@
 		});
 	}
 
-	var tasks = [];
-	var done = 0;
-	function loadCSV(){
-		fs.readFile('./data.csv', function(error, csvData){
-			csv()
-				.from.string(
-					csvData,
-					{}
-				)
-				.to.array(function(data){
-					for( var i in data ){
-						var row = {};
-						var idx = 0;
-						row['label'] = data[i][idx++];
-						row['templateName'] = data[i][idx++];
-						row['width'] = Number(data[i][idx++]);
-						row['height'] = Number(data[i][idx++]);
-						row['fileName'] = require('crypto').createHash('md5').update(row['label'], 'utf8').digest('hex')+'.png';
-						tasks.push(row);
-					}
-					startGenerateImages();
-				})
-			;
-
-		});
-	}
-
-	function startGenerateImages(){
-		console.log(tasks.length);
-
-		for( var i in tasks ){
-			var row = tasks[i];
-			console.log(row);
-
-			webCapFile(
-				row,
-				'http://127.0.0.1:'+options.port+'/'+row.templateName+'.html?label='+encodeURIComponent( row.label ),
-				'./output/'+row.fileName,
-				row.width,
-				row.height,
-				row.width,
-				row.height,
-				function(row){
-					console.log(row.label + ' done.');
-					done ++;
-					if( tasks.length == done ){
-						console.log( 'all task done.'+"\n" );
-						console.log( 'exit;'+"\n" );
-						process.exit();
-					}
-				}
-			);
-		}
-	}
-
 	if( options.mode != 'preview' ){
+		// previewモードの時は、サーバーだけ立ち上げる
 		loadCSV();
+	}else{
+		console.log('Standby as "preview" mode.');
+		console.log('Access http://127.0.0.1:'+conf.port+'/ on your browser.');
+		console.log('Press Ctrl+C to exit.');
+		console.log('');
 	}
 
 }).call(this);
---
+++ readme.md
@@ -0,0 +1,60 @@
+# headingImageGenerator.js
+
+このスクリプトは、CSVの設定に従って見出し画像を生成します。
+
+## 使い方
+
+### 1. headingImageGenerator.js をインストール
+
+```
+$ cd (your directory)
+$ git clone https://github.com/tomk79/headingImageGenerator.git
+$ cd headingImageGenerator
+$ npm install
+```
+### 2. data.csv を編集
+
+```
+$ vim data.csv
+```
+
+### 3. テンプレートを編集
+
+```
+$ vim htdocs/index.html
+```
+
+### 4. 編集したテンプレートの表示を確認
+
+```
+$ node headingImageGenerator.js port=8080 mode=preview
+```
+
+この状態で、ブラウザで http://127.0.0.1:8080/ へアクセスします。
+プレビューを修了するときは、Ctrl+C を押します。
+
+### 5. 見出し画像を出力する
+
+```
+$ node headingImageGenerator.js port=8080
+```
+
+コマンドが修了するまでしばらく待ちます。
+カレントディレクトリにoutputディレクトリが作成され、その中にPNG画像が出力されます。
+
+
+
+## オプション
+
+- port - スクリプト内で立ち上げるサーバーのポート番号。省略時 80 を使用。
+- mode - 実行モード。preview を指定すると、プレビューモードでサーバーを起動。省略時に、書き出しを実行する。
+- unit - 一度に投げるキューの数を指定。省略時は 1。同時に複数投げた方が、全体の処理は早く終る場合がある。
+- pathCsv - CSVのパスを指定。省略時、カレントディレクトリの ./data.csv を読み込む。
+- pathOutput - 出力先ディレクトリを指定。省略時、カレントディレクトリに output ディレクトリを作成して書き出す。
+
+## change log
+
+### headingImageGenerator.js 1.0.0 (2014/\*\*/\*\*)
+
+- 初版リリース
+
